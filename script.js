ba4821227977b06976afdcc584ced7fe37d8d69a
VJ <139743089+Vinayak-Joshi15@users.noreply.github.com>
2024-05-05 13:11:54 +0530

    Improved the JS

--- script.js
+++ script.js
@@ -1,202 +1,61 @@
 const screen = document.querySelector(".display");
-let a = "";
-let b = "";
-let symbol1,symbol2;
 
-let calc = 0;
+a = "";
+sym = "=";
+flag = false;
+num1 = 0;
+num2 = 0;
 
-let num = document.createElement("h1");
-num.textContent = a ? a: 0;
-screen.appendChild(num);
-
-function calculator(symbol1,b){
-    if(symbol1 == '+'){
-        calc += parseInt(b);
-    }else if(symbol1 == '-'){
-        calc -= parseInt(b);
-    }else if(symbol1 == '/'){
-        calc /= parseInt(b);
-    }else if(symbol1 == '*'){
-        calc *= parseInt(b);
-    }else if(symbol1 == 'mod'){
-        calc %= parseInt(b);
-    }else{
-        calc = parseInt(b);
-    }
-    a = 0;
-    console.log(calc);
-    
-}
+let disp = document.createElement("h1");
+    disp.textContent = 0;
+    screen.appendChild(disp);
 
 function refresh(a){
-    let num = document.createElement("h1");
-    num.textContent = a ? a: 0;
-    screen.appendChild(num);
-    
+    screen.removeChild(screen.firstChild);
+    let disp = document.createElement("h1");
+    disp.textContent = a;
+    screen.appendChild(disp);
 }
 
-const clear = document.querySelector(".clear");
+const numbers = document.querySelectorAll(".numbers");
 
-clear.addEventListener('click',()=>{
-    a = '0';
-    screen.removeChild(screen.firstChild);
-    console.log("Works");
-    refresh(a);
-    calc = 0;
-});
+function worker(btn){
+    btn.addEventListener('click',() => {
+        a += btn.textContent;
+        refresh(a);
+    })
+}
+numbers.forEach(worker);
 
-const one = document.querySelector("#one");
-const two = document.querySelector("#two");
-const three = document.querySelector("#three");
-const four = document.querySelector("#four");
-const five = document.querySelector("#five");
-const six = document.querySelector("#six");
-const seven = document.querySelector("#seven");
-const eight = document.querySelector("#eight");
-const nine = document.querySelector("#nine");
-const zero = document.querySelector("#zero");
+function signMaker(btn){
+    btn.addEventListener('click',() => {
+        sym = btn.textContent;
+        console.log(sym);
+        if(!flag){
+            num1 = parseInt(a);
+            flag = true;
+        }else{
+            num2 = parseInt(a);
+        }
+        a = "";
+        refresh("_");
+        console.log(num1,num2);
+    })
+}
 
+const signs = document.querySelectorAll(".sign");
 
-one.addEventListener('click',() => {
-    a += '1';
-    screen.removeChild(screen.firstChild);
-    console.log("Works");
-    refresh(a);
-});
-
-two.addEventListener('click',() => {
-    a += '2';
-    screen.removeChild(screen.firstChild);
-    console.log("Works");
-    refresh(a);
-});
-
-three.addEventListener('click',() => {
-    a += '3';
-    screen.removeChild(screen.firstChild);
-    console.log("Works");
-    refresh(a);
-});
-
-four.addEventListener('click',() => {
-    a += '4';
-    screen.removeChild(screen.firstChild);
-    console.log("Works");
-    refresh(a);
-});
-
-five.addEventListener('click',() => {
-    a += '5';
-    screen.removeChild(screen.firstChild);
-    console.log("Works");
-    refresh(a);
-});
-
-six.addEventListener('click',() => {
-    a += '6';
-    screen.removeChild(screen.firstChild);
-    console.log("Works");
-    refresh(a);
-});
-
-seven.addEventListener('click',() => {
-    a += '7';
-    screen.removeChild(screen.firstChild);
-    console.log("Works");
-    refresh(a);
-});
-
-eight.addEventListener('click',() => {
-    a += '8';
-    screen.removeChild(screen.firstChild);
-    console.log("Works");
-    refresh(a);
-});
-
-nine.addEventListener('click',() => {
-    a += '9';
-    screen.removeChild(screen.firstChild);
-    console.log("Works");
-    refresh(a);
-});
-
-zero.addEventListener('click',() => {
-    a += '0';
-    screen.removeChild(screen.firstChild);
-    console.log("Works");
-    refresh(a);
-});
-
-const plus = document.querySelector(".plus");
-const minus = document.querySelector(".minus");
-const multiply = document.querySelector(".multiply");
-const divide = document.querySelector(".divide");
-
-plus.addEventListener('click',() => {
-    symbol1 = symbol2;
-    symbol2 = '+';
-    b = a;
-    a = '+';
-    screen.removeChild(screen.firstChild);
-    console.log("Works");
-    calculator(symbol1,b);
-    refresh(a);
-});
-
-minus.addEventListener('click',() => {
-    b = a;
-    a = '-';
-    symbol1 = symbol2;
-    symbol2 = '-';
-    screen.removeChild(screen.firstChild);
-    console.log("Works");
-    calculator(symbol1,b);
-    refresh(a);
-});
-
-divide.addEventListener('click',() => {
-    b = a;
-    a = '/';
-    symbol1 = symbol2;
-    symbol2 = '/';
-    screen.removeChild(screen.firstChild);
-    console.log("Works");
-    calculator(symbol1,b);
-    refresh(a);
-});
-
-multiply.addEventListener('click',() => {
-    b = a;
-    a = '*';
-    symbol1 = symbol2;
-    symbol2 = '*';
-    screen.removeChild(screen.firstChild);
-    console.log("Works");
-    calculator(symbol1,b);
-    refresh(a);
-});
+signs.forEach(signMaker);
 
 const equal = document.querySelector(".equals");
 
 equal.addEventListener('click',() => {
-    b = a;
-    symbol1 = symbol2;
-    screen.removeChild(screen.firstChild);
-    console.log("Works");
-    calculator(symbol1,b);
-    refresh(calc);
+    if(sym == '+'){refresh(num1+num2);}
+    else if(sym == '-'){refresh(num1-num2);}
+    else if(sym == '*'){refresh(num1*num2);}
+    else if(sym == '/'){refresh(num1/num2);}
+    else if(sym == 'Mod'){refresh(num1%num2);}
+    else{refresh(num1+num2);}
 });
 
-const mod = document.querySelector(".mod");
-
-mod.addEventListener('click',() => {
-    b = a;
-    a = '*';
-    symbol1 = symbol2;
-    symbol2 = 'mod';
-    screen.removeChild(screen.firstChild);
-    console.log("Works");
-    calculator(symbol1,b);
-    refresh(a);
-});
 
